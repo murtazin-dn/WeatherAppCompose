62e059c961ce0bf6e1fc8b20922d40ece838bf70
мега шланг <megashlanggggg@gmail.com>
2024-01-10 01:08:24 +0500

    home screen

--- app/src/main/java/com/example/weatherappcompose/ui/screens/home/views/HomeViewDisplay.kt
+++ app/src/main/java/com/example/weatherappcompose/ui/screens/home/views/HomeViewDisplay.kt
@@ -1,20 +1,27 @@
 package com.example.weatherappcompose.ui.screens.home.views
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherappcompose.R
 import com.example.weatherappcompose.ui.model.ForecastWeather
 import com.example.weatherappcompose.ui.model.base.AirQuality
@@ -29,6 +36,7 @@ import com.example.weatherappcompose.ui.model.base.Forecastday
 import com.example.weatherappcompose.ui.model.base.Hour
 import com.example.weatherappcompose.ui.model.base.Location
 import com.example.weatherappcompose.ui.screens.home.model.HomeViewState
+import com.example.weatherappcompose.ui.theme.interFamily
 import com.example.weatherappcompose.ui.utils.ext.dateToDate
 
 @Composable
@@ -53,21 +61,64 @@ fun HomeViewDisplay(
                     .padding(20.dp)
             ){
                 Icon(
+                    modifier = Modifier.width(40.dp),
                     painter = painterResource(id = R.drawable.ic_hamburger),
-                    contentDescription = "Hamburger"
+                    contentDescription = "Hamburger",
+                    tint = Color.White
                 )
-                Column() {
-                    Row {
+                Column(
+                    Modifier.weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row() {
                         Text(
+                            color = Color.White,
+                            fontFamily = interFamily,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 17.sp,
                             text = state.weather.location.region
                         )
                         Icon(
                             painter = painterResource(id = R.drawable.ic_expand_more),
-                            contentDescription = "Expand more")
+                            contentDescription = "Expand more",
+                            tint = Color.White
+                        )
                     }
-                    Text(text = state.weather.current.last_updated.dateToDate())
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp),
+                        fontFamily = interFamily,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        text = state.weather.current.last_updated.dateToDate()
+                    )
                 }
+                Box(modifier = Modifier.width(50.dp))
             }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 100.dp),
+                horizontalArrangement = Arrangement.Center
+
+            ) {
+                Text(
+                    fontFamily = interFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 160.sp,
+                    color = Color.White,
+                    text = state.weather.current.temp_c.toInt().toString()
+                )
+                Text(
+                    modifier = Modifier.padding(top = 30.dp),
+                    fontFamily = interFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 70.sp,
+                    color = Color.White,
+                    text = "°"
+                )
+            }
+
         }
 
     }
---
+++ app/src/main/java/com/example/weatherappcompose/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.example.weatherappcompose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.weatherappcompose.R
+
+val interFamily = FontFamily(
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
